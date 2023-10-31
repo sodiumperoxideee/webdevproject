b27612847cb0b43100d9693425193d833536d112
chlorioineychee <makseyndelacruz@gmail.com>
2023-10-31 15:01:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,45 @@
     include_once("dbConnection/mysqlconfig_connection.php");
     include_once("functions/fetch.php");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            border: 1px solid #CCCCCC;
+        }
+    </style>
+</head>
+<body>
+    <h1>My Subjects</h1>
+    <a href="forms/addform.php">Add Subject</a>
+    <br><br>
+    <table>
+        <tr>
+            <td>ID</td>
+            <td>Subject Code</td>
+            <td>Subject Name</td>
+            <td>Action</td>
+        </tr>
+        <?php
+            while($res = mysqli_fetch_array($result)) {
+                echo "<tr>";
+                echo "<td>".$res['subject_id']."</td>";
+                echo "<td>".$res['subject_code']."</td>";
+                echo "<td>".$res['subject_name']."</td>";
+                echo "<td><a href=\"forms/editform.php?id=$res[subject_id]\">Edit</a> |
+                        <a href=\"forms/delete.php?id=$res[subject_id]\"
+                        onclick=\"return confirm('Are you sure you want to delete?')\"Delete</a></td>";
+            }    
+        ?>
+    </table>
+</body>
+</html>
