cbd2b3ed32896797f0ec85ac945e8858213f3514
chlorioineychee <makseyndelacruz@gmail.com>
2023-10-31 16:25:36 +0800

    Update editform.php

---
+++ forms/editform.php
@@ -0,0 +1,40 @@
+<?php
+    include_once("../dbConnection/mysqlconfig_connection.php");
+
+    $id = $_GET["id"];
+
+    $result = mysqli_query($dbc,"SELECT * FROM tblsubject WHERE subject_id=$id");
+    while($res = mysqli_fetch_array($result)) {
+        $code = $res["_subject_code"];
+        $name = $res["_subject_name"];
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Data</title>
+</head>
+<body>
+    <h1>Edit Subject</h1>
+    <a href="../index.php">Home</a>
+    <br/><br/>
+    <form action="../functions/edit.php" method="post" name="form1">
+        <table>
+            <tr>
+                <td>Subject Code</td>
+                <td><input type="text" name="code" value="<?php echo $code;?>"></td>
+            </tr>
+            <tr>
+                <td>Subject Name</td>
+                <td><input type="text" name="name" value="<?php echo $name;?>"></td>
+            </tr>
+            <tr>
+                <td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
+                <td><input type="submit" name="update" value="Update"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
