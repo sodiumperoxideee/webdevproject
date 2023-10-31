fd56bcfdf82a081bcee0d8a341af9f663322e2cd
chlorioineychee <makseyndelacruz@gmail.com>
2023-10-31 14:40:48 +0800

    added an indentions

--- dbConnection/mysqlconfig_connection.php
+++ dbConnection/mysqlconfig_connection.php
@@ -1,10 +1,10 @@
 <?php
-DEFINE ('DB_USER','root');
-DEFINE ('DB_PASSWORD','');
-DEFINE ('DB_HOST','localhost');
-DEFINE ('DB_NAME','dbschedule');
+    DEFINE ('DB_USER','root');
+    DEFINE ('DB_PASSWORD','');
+    DEFINE ('DB_HOST','localhost');
+    DEFINE ('DB_NAME','dbschedule');
 
-$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-OR die('Could not connect to MySQL: ' . mysqli_connect_error());
+    $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+    OR die('Could not connect to MySQL: ' . mysqli_connect_error());
 
 ?>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php 
+include_once('dbConnection/mysqlconfig_connection.php');
+?>
